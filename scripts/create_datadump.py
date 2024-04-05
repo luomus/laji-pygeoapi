5c8cd47b411230b9ca8d850c436773cee0a38f48
Alpo Turunen <Localadmin_alpoturu@lh2-978-26536.ad.helsinki.fi>
2024-04-05 15:04:04 +0300

    edited script

--- scripts/create_datadump.py
+++ scripts/create_datadump.py
@@ -7,9 +7,9 @@ geojson_file = r'10000_species.geo.json'
 
 # Database connection parameters
 db_params = {
-    'dbname': 'my_geospatial_db',
+    'dbname': 'my_geospatial_db2',
     'user': 'postgres',
-    'password': '',
+    'password': '', #ADD PASSWORD
     'host': 'localhost',
     'port': '5432'
 }
@@ -17,16 +17,21 @@ db_params = {
 # Table name in the database
 table_name = 'occurrence_data'
 
-# Read GeoJSON file into a GeoDataFrame
-gdf = gpd.read_file(geojson_file)
-print(gdf.head())
-
 # Connect to the PostGIS database
 engine = create_engine(f"postgresql+psycopg2://{db_params['user']}:{db_params['password']}@{db_params['host']}:{db_params['port']}/{db_params['dbname']}")
 
-# Create a database if not exist
+# Create a new database if not exist
 if not database_exists(engine.url):
     create_database(engine.url)
+
+with engine.connect() as connection:
+    connection.execute(text('CREATE EXTENSION IF NOT EXISTS postgis;'))
+    connection.commit()
+
+# Read GeoJSON file into a GeoDataFrame and add id column
+gdf = gpd.read_file(geojson_file)
+gdf['id'] = range(1, len(gdf) + 1)
+print(gdf.head())
 
 # Insert data into the PostGIS database
 gdf.to_postgis(table_name, engine, if_exists='replace', index=False)
