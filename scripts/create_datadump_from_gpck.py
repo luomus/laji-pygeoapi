f221dd23513d645bce66194680ef4fb78067cc9f
Alpo Turunen <Localadmin_alpoturu@lh2-978-26536.ad.helsinki.fi>
2024-04-12 11:41:26 +0300

    add automatic configuration edit function

--- scripts/create_datadump_from_gpck.py
+++ scripts/create_datadump_from_gpck.py
@@ -6,6 +6,8 @@ import re
 print("start")
 # Path to the GeoJSON file
 gpkd = r'scripts\HBF_86582_geo.gpkg'
+template_resource = r'scripts\template_resource.txt'
+pygeoapi_config = r'pygeoapi-config.yml'
 
 # Database connection parameters
 db_params = {
@@ -21,11 +23,12 @@ gdf = gpd.read_file(gpkd, rows=10000)
 crs = gdf.crs
 gdf = gdf[['record_id', 'reported_name', 'common_name_finnish', 'scientific_name_interpreted', 'informal_groups_finnish', 'informal_groups_english', 'geometry']]
 
-# Split 
+# Split column so species can be classified based on main group e.g. "Birds"
 gdf['class'] = gdf['informal_groups_english'].str.split(';', expand=True)[0]
 print(gdf.head())
 
 # Connect to the PostGIS database
+
 engine = create_engine(f"postgresql+psycopg2://{db_params['user']}:{db_params['password']}@{db_params['host']}:{db_params['port']}/{db_params['dbname']}")
 print("Connection created")
 
@@ -38,30 +41,65 @@ with engine.connect() as connection:
     connection.execute(text('CREATE EXTENSION IF NOT EXISTS postgis;'))
     connection.commit()
 
-# Function to clean the group value and return a table name
+
 def clean_table_name(group_value):
+    ''' Function to clean and return a table name '''
+    
+    # If name not exists
     if group_value is None or group_value == '':
         return 'unclassified'
-    cleaned_value = re.sub(r'[^\w\s]', '', group_value)  # Remove non-alphanumeric characters
-    cleaned_value = cleaned_value.replace(' ', '_')  
+    
+    # Remove non-alphanumeric characters and white spaces
+    cleaned_value = re.sub(r'[^\w\s]', '', group_value).replace(' ', '_')   
+
+    # Shorten the name
     if len(cleaned_value) > 40:
         cleaned_value = cleaned_value[:40]
-        print(f"Shorten name is {cleaned_value}")
+
     return f'{cleaned_value}'
 
-# Iterate over unique values of the "informal_groups_english" attribute
-for group_value in gdf['class'].unique():
-    # Filter the GeoDataFrame for the current group
-    group_gdf = gdf[gdf['class'] == group_value]
-    n_rows = len(group_gdf)
+def get_bbox(sub_gdf):
+    minx, miny, maxx, maxy = sub_gdf.geometry.total_bounds
+    return [minx, miny, maxx, maxy]
 
-    # Define the table name based on the group name
+def add_to_pygeoapi_config(template_resource, template_params, pygeoapi_config):
+    '''This function adds data sets to the pygeoapi config file'''
+
+    # Read the template file
+    with open(template_resource, "r") as file:
+        template = file.read()
+
+    # Replace placeholders with real values
+    for key, value in template_params.items():
+        template = template.replace(key, value)
+   
+    # Append filled tempalte to the config file
+    with open(pygeoapi_config, "a") as file:
+        file.write(template)
+        print(f"Table {template_params['<placeholder_table_name>']} added to the pygeoapi config file")
+
+# Iterate over unique values of the "class" attribute
+for group_value in gdf['class'].unique():
+    
+    # Filter the GeoDataFrame based on species
+    sub_gdf = gdf[gdf['class'] == group_value]
+    
+    # Get cleaned table name, bounding box (bbox) and number of rows (n_rows)
     table_name = clean_table_name(group_value)
+    bbox = get_bbox(sub_gdf)
+    n_rows = len(sub_gdf)
+
+    template_params = {
+        "<placeholder_table_name>": table_name,
+        "<placeholder_bbox>": str(bbox)
+    }
+
+    add_to_pygeoapi_config(template_resource, template_params, pygeoapi_config)
 
     # Create a PostGIS table 
-    group_gdf.to_postgis(table_name, engine, if_exists='replace', schema='public', index=False)
-
+    sub_gdf.to_postgis(table_name, engine, if_exists='replace', schema='public', index=False)
     print(f"In total {n_rows} rows of {table_name} inserted to the database")
 
 
-print("All data inserted successfully into the PostGIS database.")
+print("All data inserted successfully into the PostGIS database and pygeoapi config file.")
+print("API is ready to use.")
