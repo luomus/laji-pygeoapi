89e87e31573d53ea65d32fd50dd22fe8f66f9865
Alpo Turunen <Alpo.96@gmail.com>
2024-04-22 16:50:47 +0300

    add taxonomy info

--- scripts/create_datadump_from_url.py
+++ scripts/create_datadump_from_url.py
@@ -6,14 +6,17 @@ import pandas as pd
 from tqdm import tqdm
 import requests
 import pyogrio, psycopg2, geoalchemy2
+import json
 
 pd.options.mode.copy_on_write = True
 
 print("Start")
 # Path to the GeoJSON file
-data_url = r'https://laji.fi/api/warehouse/query/unit/list?administrativeStatusId=MX.finlex160_1997_appendix4_2021,MX.finlex160_1997_appendix4_specialInterest_2021,MX.finlex160_1997_appendix2a,MX.finlex160_1997_appendix2b,MX.finlex160_1997_appendix3a,MX.finlex160_1997_appendix3b,MX.finlex160_1997_appendix3c,MX.finlex160_1997_largeBirdsOfPrey,MX.habitatsDirectiveAnnexII,MX.habitatsDirectiveAnnexIV,MX.birdsDirectiveStatusAppendix1,MX.birdsDirectiveStatusMigratoryBirds&redListStatusId=MX.iucnCR,MX.iucnEN,MX.iucnVU,MX.iucnNT&countryId=ML.206&time=1990-01-01/&aggregateBy=gathering.conversions.wgs84Grid05.lat,gathering.conversions.wgs84Grid1.lon&onlyCount=false&individualCountMin=0&coordinateAccuracyMax=1000&page=1&pageSize=10000&taxonAdminFiltersOperator=OR&collectionAndRecordQuality=PROFESSIONAL:EXPERT_VERIFIED,COMMUNITY_VERIFIED,NEUTRAL,UNCERTAIN;HOBBYIST:EXPERT_VERIFIED,COMMUNITY_VERIFIED,NEUTRAL;AMATEUR:EXPERT_VERIFIED,COMMUNITY_VERIFIED;&geoJSON=true&featureType=ORIGINAL_FEATURE'
-#data_url = r'test_data\10000_virva_data.json'
-taxon_file = r'scripts\taxon-export.tsv'
+occurrence_url = r'https://laji.fi/api/warehouse/query/unit/list?administrativeStatusId=MX.finlex160_1997_appendix4_2021,MX.finlex160_1997_appendix4_specialInterest_2021,MX.finlex160_1997_appendix2a,MX.finlex160_1997_appendix2b,MX.finlex160_1997_appendix3a,MX.finlex160_1997_appendix3b,MX.finlex160_1997_appendix3c,MX.finlex160_1997_largeBirdsOfPrey,MX.habitatsDirectiveAnnexII,MX.habitatsDirectiveAnnexIV,MX.birdsDirectiveStatusAppendix1,MX.birdsDirectiveStatusMigratoryBirds&redListStatusId=MX.iucnCR,MX.iucnEN,MX.iucnVU,MX.iucnNT&countryId=ML.206&time=1990-01-01/&aggregateBy=gathering.conversions.wgs84Grid05.lat,gathering.conversions.wgs84Grid1.lon&onlyCount=false&individualCountMin=0&coordinateAccuracyMax=1000&page=1&pageSize=10000&taxonAdminFiltersOperator=OR&collectionAndRecordQuality=PROFESSIONAL:EXPERT_VERIFIED,COMMUNITY_VERIFIED,NEUTRAL,UNCERTAIN;HOBBYIST:EXPERT_VERIFIED,COMMUNITY_VERIFIED,NEUTRAL;AMATEUR:EXPERT_VERIFIED,COMMUNITY_VERIFIED;&geoJSON=true&featureType=ORIGINAL_FEATURE'
+#occurrence_url = r'test_data\10000_virva_data.json'
+taxon_id_url= r'https://laji.fi/api/taxa/MX.37600/species?onlyFinnish=true&selectedFields=id,vernacularName,scientificName,informalTaxonGroups&lang=multi&page=1&pageSize=1000&sortOrder=taxonomic'
+taxon_name_url = r'https://laji.fi/api/informal-taxon-groups?pageSize=1000'
+#taxon_file = r'scripts\taxon-export.tsv'
 template_resource = r'scripts\template_resource.txt'
 pygeoapi_config = r'pygeoapi-config.yml'
 
@@ -91,21 +94,70 @@ def add_to_pygeoapi_config(template_resource, template_params, pygeoapi_config):
         file.write(template)
         print(f"Table {template_params['<placeholder_table_name>']} added to the pygeoapi config file")
 
-# Loop over API pages and read the data 
-print("Retrieving data from the API...")
-# last_page = get_last_page(data_url)
-last_page = 1
-gdf = gpd.GeoDataFrame()
-for page_no in tqdm(range(1,last_page+1)):
-    next_page = data_url.replace('page=1', f'page={page_no}')
-    next_gdf = gpd.read_file(next_page, engine="pyogrio")
-    gdf = pd.concat([gdf, next_gdf])
+def get_occurrence_data(data_url, pages="all"):
+    # Loop over API pages and return the data as GeoDataFrame
+    print("Retrieving occurrence data from the API...")
+    if pages == 'all':
+        last_page = get_last_page(data_url)
+    else:
+        last_page = int(pages)
+
+    gdf = gpd.GeoDataFrame()
+    for page_no in tqdm(range(1,last_page+1)):
+        next_page = data_url.replace('page=1', f'page={page_no}')
+        next_gdf = gpd.read_file(next_page)
+        gdf = pd.concat([gdf, next_gdf])
+    gdf = gdf[['unit.linkings.taxon.id','unit.linkings.taxon.scientificName','unit.linkings.taxon.vernacularName.en','unit.unitId','gathering.displayDateTime','geometry']]
+    return gdf
+
+def get_taxon_data(taxon_id_url, taxon_name_url, pages='all'):
+    # Loop over API pages and return data as Dataframe
+    print("Retrieving taxon data from the API...")
+    if pages == 'all':
+        last_page = get_last_page(taxon_id_url)
+    else:
+        last_page = int(pages)
+
+    id_df = pd.DataFrame()
+    for page_no in tqdm(range(1, last_page + 1)):
+        next_page = taxon_id_url.replace('page=1', f'page={page_no}')
+        response = requests.get(next_page)
+        if response.status_code == 200:
+            json_data_results = response.json().get('results', [])
+            next_df = pd.json_normalize(json_data_results)
+            id_df = pd.concat([id_df, next_df], ignore_index=True)
+        else:
+            print(f"Failed to fetch data from page {page_no}. Status code: {response.status_code}")
+
+    def find_main_taxon(row):
+        # Extract numeric part and convert to integer for each value in the list
+        if type(row) is list:
+            numeric_values = [int(value.split('.')[1]) for value in row]
+            min_value = 'MVL.' + str(min(numeric_values))
+        else:
+            min_value = str(row)
+        # Find the minimum value
+        
+        return min_value
+
+    # Apply the function to each row and store the result in a new column
+    id_df['mainTaxon'] = id_df['informalTaxonGroups'].apply(find_main_taxon)
+    print(id_df['mainTaxon'])
+
+    response = requests.get(taxon_name_url)
+    json_data_results = response.json().get('results', [])
+    name_df = pd.json_normalize(json_data_results)
+
+    df = pd.merge(id_df, name_df, left_on='mainTaxon', right_on='id')
+    return df
+
+# Get data sets
+gdf = get_occurrence_data(occurrence_url, pages=1)
+taxon_df = get_taxon_data(taxon_id_url, taxon_name_url, pages='all')
 
 # Merge taxonomy information to the geodataframe
-taxonomy_df = pd.read_table(taxon_file, sep='\t')
-gdf['unit.linkings.taxon.id'] = 'http://tun.fi/MX.' + gdf['unit.linkings.taxon.id'].str.extract('MX\.(\d+)')
-taxonomy_df['Identifier'] = 'http://tun.fi/MX.' + taxonomy_df['Identifier'].str.extract('MX\.(\d+)')
-gdf = gdf.merge(taxonomy_df, left_on='unit.linkings.taxon.id', right_on='Identifier', how='left')
+gdf['unit.linkings.taxon.id'] = gdf['unit.linkings.taxon.id'].str.extract('(MX\.\d+)')
+gdf = gdf.merge(taxon_df, left_on='unit.linkings.taxon.id', right_on='id_x', how='left')
 
 # Connect to the PostGIS database
 print("Creating database connection...")
@@ -122,12 +174,12 @@ with engine.connect() as connection:
 
 # Iterate over unique values of the "class" attribute
 tot_rows = 0
-no_family_name = gdf[gdf['Class, Scientific name'].isnull()]
+no_family_name = gdf[gdf['name'].isnull()]
 
 print("Looping over all species classes...")
-for group_name in gdf['Class, Scientific name'].unique():
+for group_name in gdf['name'].unique():
     # Filter the GeoDataFrame based on species
-    sub_gdf = gdf[gdf['Class, Scientific name'] == group_name]
+    sub_gdf = gdf[gdf['name'] == group_name]
 
     # Get cleaned table name, bounding box (bbox) and number of rows (n_rows)
     table_name = clean_table_name(group_name)
