f240c953baa9607ada918c6965c2b903e5acbf63
Alpo Turunen <Alpo.96@gmail.com>
2024-04-25 07:09:19 +0300

    add multiprocessing and crs support

--- scripts/create_datadump_from_url.py
+++ scripts/create_datadump_from_url.py
@@ -6,9 +6,12 @@ import pandas as pd
 from tqdm import tqdm
 import requests
 import pyogrio, psycopg2, geoalchemy2
-import json
+import os
+from dotenv import load_dotenv
+import concurrent.futures
 
 pd.options.mode.copy_on_write = True
+gpd.options.io_engine = "pyogrio" # Faster way to read data
 
 print("Start")
 # Path to the GeoJSON file
@@ -21,11 +24,12 @@ template_resource = r'scripts\template_resource.txt'
 pygeoapi_config = r'pygeoapi-config.yml'
 lookup_table = r'scripts\lookup_table_columns.csv'
 
-# Database connection parameters
+# Database connection parameters from the secret .env file
+load_dotenv()
 db_params = {
-    'dbname': 'my_geospatial_db',
-    'user': 'postgres',
-    'password': 'admin123',
+    'dbname': os.getenv('POSTGRES_DB'),
+    'user': os.getenv('POSTGRES_USER') ,
+    'password': os.getenv('POSTGRES_PASSWORD') ,
     'host': 'localhost',
     'port': '5433'
 }
@@ -53,14 +57,14 @@ def clean_table_name(group_name):
 
     return f'{cleaned_name}'
 
-def clean_column_names(gdf, lookup_table):
+def column_names_as_dwc(gdf, lookup_table):
     # Load the lookup table CSV into a DataFrame
     lookup_df = pd.read_csv(lookup_table, sep=';', header=0)
 
-    # Map all column names according to the names in the lookup table
+    # Map all column names according to the Darwin Core names in the lookup table
     column_mapping = {}
-    for index, row in lookup_df.iterrows():
-        column_mapping[row['finbif_api_var']] = row['translated_var']
+    for _, row in lookup_df.iterrows():
+        column_mapping[row['finbif_api_var']] = row['dwc']
 
     # Rename columns based on the mapping
     gdf.rename(columns=column_mapping, inplace=True)
@@ -72,7 +76,7 @@ def get_bbox(sub_gdf):
     return [minx, miny, maxx, maxy]
 
 def get_min_and_max_dates(sub_gdf):
-    dates = sub_gdf['formatted_date_time']
+    dates = sub_gdf['eventDateTimeDisplay']
     # Convert the 'formatted_date_time' column to pandas datetime format
     try:
         dates = pd.to_datetime(dates.str.split(' ', expand=True).iloc[:, 0] + 'T00:00:00Z')
@@ -106,7 +110,12 @@ def add_to_pygeoapi_config(template_resource, template_params, pygeoapi_config):
         file.write(template)
         print(f"Table {template_params['<placeholder_table_name>']} added to the pygeoapi config file")
 
-def get_occurrence_data(data_url, pages="all"):
+def download_page(data_url, page_no):
+    data_url = data_url.replace('page=1', f'page={page_no}')
+    gdf = gpd.read_file(data_url)
+    return gdf
+
+def get_occurrence_data(data_url, multiprocessing=True, pages="all"):
     # Loop over API pages and return the data as GeoDataFrame
     print("Retrieving occurrence data from the API...")
     if pages == 'all':
@@ -115,10 +124,16 @@ def get_occurrence_data(data_url, pages="all"):
         last_page = int(pages)
 
     gdf = gpd.GeoDataFrame()
-    for page_no in tqdm(range(1,last_page+1)):
-        next_page = data_url.replace('page=1', f'page={page_no}')
-        next_gdf = gpd.read_file(next_page)
-        gdf = pd.concat([gdf, next_gdf])
+    if multiprocessing==True:
+        with concurrent.futures.ProcessPoolExecutor() as executor:
+            futures = [executor.submit(download_page, data_url, page_no) for page_no in range(1, last_page + 1)]
+            for future in concurrent.futures.as_completed(futures):
+                gdf = pd.concat([gdf, future.result()], ignore_index=True)
+    else:
+        for page_no in tqdm(range(1,last_page+1)):
+            next_gdf = download_page(data_url, page_no)
+            gdf = pd.concat([gdf, next_gdf])
+
     gdf = gdf[['unit.linkings.taxon.id','unit.linkings.taxon.scientificName','unit.linkings.taxon.vernacularName.en','unit.unitId','gathering.displayDateTime','geometry']]
     return gdf
 
@@ -162,64 +177,68 @@ def get_taxon_data(taxon_id_url, taxon_name_url, pages='all'):
     df = pd.merge(id_df, name_df, left_on='mainTaxon', right_on='id')
     return df
 
-# Get data sets
-gdf = get_occurrence_data(occurrence_url, pages=1)
-taxon_df = get_taxon_data(taxon_id_url, taxon_name_url, pages='all')
+def main():
+    # Get the data sets
+    gdf = get_occurrence_data(occurrence_url, multiprocessing=True, pages=10)
+    taxon_df = get_taxon_data(taxon_id_url, taxon_name_url, pages='all')
 
-# Merge taxonomy information to the occurrence data
-gdf['unit.linkings.taxon.id'] = gdf['unit.linkings.taxon.id'].str.extract('(MX\.\d+)')
-gdf = gdf.merge(taxon_df, left_on='unit.linkings.taxon.id', right_on='id_x', how='left')
+    # Merge taxonomy information to the occurrence data
+    gdf['unit.linkings.taxon.id'] = gdf['unit.linkings.taxon.id'].str.extract('(MX\.\d+)')
+    gdf = gdf.merge(taxon_df, left_on='unit.linkings.taxon.id', right_on='id_x', how='left')
 
-# Clean column names
-gdf = clean_column_names(gdf, lookup_table)
+    # Translate column names to comply with the darwin core standard
+    gdf = column_names_as_dwc(gdf, lookup_table)
 
-# Connect to the PostGIS database
-print("Creating database connection...")
-engine = create_engine(f"postgresql+psycopg2://{db_params['user']}:{db_params['password']}@{db_params['host']}:{db_params['port']}/{db_params['dbname']}")
+    # Connect to the PostGIS database
+    print("Creating database connection...")
+    engine = create_engine(f"postgresql+psycopg2://{db_params['user']}:{db_params['password']}@{db_params['host']}:{db_params['port']}/{db_params['dbname']}")
 
-# Create a new database if not exists
-#if not database_exists(engine.url):
-#    create_database(engine.url)
+    # Create a new database if not exists
+    #if not database_exists(engine.url):
+    #    create_database(engine.url)
 
-# Add Postgis plugin if not added
-with engine.connect() as connection:
-    connection.execute(text('CREATE EXTENSION IF NOT EXISTS postgis;'))
-    connection.commit()
+    # Add Postgis plugin if not added
+    with engine.connect() as connection:
+        connection.execute(text('CREATE EXTENSION IF NOT EXISTS postgis;'))
+        connection.commit()
 
-# Iterate over unique values of the "class" attribute
-tot_rows = 0
-no_family_name = gdf[gdf['name'].isnull()]
+    # Iterate over unique values of the "class" attribute
+    tot_rows = 0
+    no_family_name = gdf[gdf['name'].isnull()]
 
-print("Looping over all species classes...")
-for group_name in gdf['name'].unique():
-    # Filter the GeoDataFrame based on species
-    sub_gdf = gdf[gdf['name'] == group_name]
+    print("Looping over all species classes...")
+    for group_name in gdf['name'].unique():
+        # Filter the GeoDataFrame based on species group
+        sub_gdf = gdf[gdf['name'] == group_name]
 
-    # Get cleaned table name, bounding box (bbox) and number of rows (n_rows)
-    table_name = clean_table_name(group_name)
-    if table_name != 'nan':
-        bbox = get_bbox(sub_gdf)
-        min_date, max_date, dates = get_min_and_max_dates(sub_gdf)
-        sub_gdf['datetimestamp'] = dates.astype(str)
-        n_rows = len(sub_gdf)
-        tot_rows += n_rows
+        # Get cleaned table name, bounding box (bbox) and number of rows (n_rows)
+        table_name = clean_table_name(group_name)
+        if table_name != 'nan':
+            bbox = get_bbox(sub_gdf)
+            min_date, max_date, dates = get_min_and_max_dates(sub_gdf)
+            sub_gdf['datetimestamp'] = dates.astype(str)
+            n_rows = len(sub_gdf)
+            tot_rows += n_rows
 
-        # Create parameters dictionary to fill the template for pygeoapi config file
-        template_params = {
-            "<placeholder_table_name>": table_name,
-            "<placeholder_bbox>": str(bbox),
-            "<placeholder_min_date>": min_date,
-            "<placeholder_max_date>": max_date
-        }
-        # Create a postgis table 
-        add_to_pygeoapi_config(template_resource, template_params, pygeoapi_config)
+            # Create parameters dictionary to fill the template for pygeoapi config file
+            template_params = {
+                "<placeholder_table_name>": table_name,
+                "<placeholder_bbox>": str(bbox),
+                "<placeholder_min_date>": min_date,
+                "<placeholder_max_date>": max_date
+            }
+            # Create a postgis table 
+            add_to_pygeoapi_config(template_resource, template_params, pygeoapi_config)
 
-        # Add postgis table information to the pygeoapi config
-        sub_gdf.to_postgis(table_name, engine, if_exists='replace', schema='public', index=False)
+            # Add postgis table information to the pygeoapi config
+            sub_gdf.to_postgis(table_name, engine, if_exists='replace', schema='public', index=False)
 
-        print(f"In total {n_rows} rows of {table_name} inserted to the database and pygeoapi config file")
+            print(f"In total {n_rows} rows of {table_name} inserted to the database and pygeoapi config file")
 
 
-print(f"In total {tot_rows} rows of data inserted successfully into the PostGIS database and pygeoapi config file.")
-print(f"Warning: in total {len(no_family_name)} species without scientific family name were discarded")
-print("API is ready to use.")
+    print(f"In total {tot_rows} rows of data inserted successfully into the PostGIS database and pygeoapi config file.")
+    print(f"Warning: in total {len(no_family_name)} species without scientific family name were discarded")
+    print("API is ready to use.")
+
+if __name__ == '__main__':
+    main()
