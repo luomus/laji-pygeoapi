c00181601f5667cd45e06f929b3d4ccd84333c5e
aorin <meeri.rannisto@gmail.com>
2024-10-04 13:41:41 +0300

    set the env values also in setup.sh script for now

--- pygeoapi/setup.sh
+++ pygeoapi/setup.sh
@@ -2,5 +2,9 @@
 
 cp /pygeoapi/catalogue.tinydb /pygeoapi/metadata_db.tinydb
 
+export PYGEOAPI_HOME=/pygeoapi
+export PYGEOAPI_CONFIG="${PYGEOAPI_HOME}/local.config.yml"
+export PYGEOAPI_OPENAPI="${PYGEOAPI_HOME}/local.openapi.yml"
+
 # generate openapi.yml
 pygeoapi openapi generate ${PYGEOAPI_CONFIG} --output-file ${PYGEOAPI_OPENAPI}
