89c45b6964c60886717caf3eb840f2c0b13b1fd1
aorin <meeri.rannisto@gmail.com>
2024-10-09 16:48:47 +0300

    don't set PYGEOAPI_CONFIG environment value multiple times

--- pygeoapi/setup.sh
+++ pygeoapi/setup.sh
@@ -2,9 +2,5 @@
 
 cp /pygeoapi/catalogue.tinydb /pygeoapi/metadata_db.tinydb
 
-export PYGEOAPI_HOME=/pygeoapi
-export PYGEOAPI_CONFIG="${PYGEOAPI_HOME}/local.config.yml"
-export PYGEOAPI_OPENAPI="${PYGEOAPI_HOME}/local.openapi.yml"
-
 # generate openapi.yml
 pygeoapi openapi generate ${PYGEOAPI_CONFIG} --output-file ${PYGEOAPI_OPENAPI}
