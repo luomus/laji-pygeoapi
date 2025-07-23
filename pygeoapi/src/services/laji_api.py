bff899bf137d9ec54b5606762b549b377a045a8f
AlpoTurunen <Alpo.96@gmail.com>
2025-07-23 16:37:00 +0300

    add retry mechanism for api key checking

--- pygeoapi/src/services/laji_api.py
+++ pygeoapi/src/services/laji_api.py
@@ -2,12 +2,15 @@ from src.app import app, cache
 import requests
 
 
-@cache.memoize(300)
+@cache.memoize(1200) # cache 1200 seconds = 20 minutes
 def get_api_key_info(api_key):
-    response = requests.get(
-        '{}warehouse/api-keys/{}'.format(app.config['LAJI_API_URL'], api_key),
-        { 'access_token': app.config['ACCESS_TOKEN'] }
-    )
-
-    data = response.json()
-    return data
+    url = '{}warehouse/api-keys/{}'.format(app.config['LAJI_API_URL'], api_key)
+    params = { 'access_token': app.config['ACCESS_TOKEN'] }
+    for _ in range(3): 
+        try:
+            response = requests.get(url, params=params)
+            response.raise_for_status()
+            return response.json()
+        except requests.RequestException:
+            continue
+    return None
